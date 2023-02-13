2c6a171a1612a32b1556525a9b266b965f7713d0
Simonhn1 <simon.lloh1@gmail.com>
2023-02-13 17:23:35 +0100

    lagt till grejer

---
+++ Entities/Book.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bibliotekssystem
+{
+    public class Book
+    {
+     public int IsbnNo { get; private set;}
+
+     public string Title { get; private set;}
+
+     public bool IsAvailable { get; set;}
+
+     public float DelayCost { get; private set;}
+
+     public Book(int isbnNo, bool isAvailable, string title, float delayCost)
+     
+     {
+
+
+            IsbnNo = isbnNo;
+            Title = title;
+            IsAvailable = isAvailable;
+            DelayCost = delayCost;
+
+       }
+
+    }
+ }
---
+++ Entities/Booking.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bibliotekssystem
+{
+     public class Booking
+    {
+    public int memberNo; { get; private set;}
+
+    public string Book; { get; private set;}
+
+    public string actualHandoverTime;
+{ get; private set;}
+
+    public string bookedHandoverTime;
+{ get; private set;}
+
+    public int employeeNo;
+{ get; private set;}
+
+    public string returnTime;
+{ get; private set;}
+    }
+}
